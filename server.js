4c701afceb675e4417fe2463ebe5dd9d094c4c8c
redoz94 <azfarbil75@gmail.com>
2020-02-11 18:15:50 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+const database = {
+	users: [
+		{
+	id: '123',
+	name: 'John',
+	password: 'cookies',
+	email: 'john@gmail.com',
+	entries: 0,
+	joined: new Date()
+		},
+		{
+	id: '124',
+	name: 'Sally',
+	password: 'bananas',
+	email: 'sally@gmail.com',
+	entries: 0,
+	joined: new Date()
+		}
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gmail.com'
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	bcrypt.compare("apples", '$2a$10$WA1x1zFITKKCBXP7R7QwVeSbpteogytCkembfR2UOXmugy7zjOr66', function(err, res) {
+    console.log('first guess', res)
+	});
+	bcrypt.compare("veggies", '$2a$10$WA1x1zFITKKCBXP7R7QwVeSbpteogytCkembfR2UOXmugy7zjOr66', function(err, res) {
+	   console.log('second guess', res)
+	});
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+	res.json('success');
+} else {
+	res.status(400).json('error logging in');
+	}
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found= true;
+			 user.entries++
+			return res.json(user.entries);
+		} 
+	})
+	if(!found) {
+		res.status(400).json('not found');
+	}
+})
+
+ app.put('/image', (req, res) => {
+ 	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found= true;
+			user.entries++
+			return res.json(user.entries );
+		} 
+	})
+	if(!found) {
+		res.status(400).json('not found');
+	}
+ })
+
+
+
+// Load hash from your password DB.
+
+
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+})
+ 
